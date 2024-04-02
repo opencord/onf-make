3aabfdfd41643d272ebbebb633345d2950ddf0de
Joey Armstrong <jarmstrong@linuxfoundation.org>
2024-04-02 15:06:03 -0400

    Update SPDX/copyright notices
    Signed-off-by: Joey Armstrong <jarmstrong@linuxfoundation.org>
    Change-Id: I728ffda93c61457cdbed429ce10de32c5b49764b

--- makefiles/virtualenv/virtualenv.mk
+++ makefiles/virtualenv/virtualenv.mk
@@ -1,12 +1,12 @@
 # -*- makefile -*-
-# -----------------------------------------------------------------------
-# Copyright 2017-2024 Open Networking Foundation Contributors
+## -----------------------------------------------------------------------
+# Copyright 2017-2024 Open Networking Foundation (ONF) and the ONF Contributors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-# http:#www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,10 +14,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # -----------------------------------------------------------------------
-# SPDX-FileCopyrightText: 2017-2024 Open Networking Foundation Contributors
-# SPDX-License-Identifier: Apache-2.0
-# -----------------------------------------------------------------------
-# Intent:
+# https://gerrit.opencord.org/plugins/gitiles/onf-make
+# ONF.makefile.version = 1.2
 # -----------------------------------------------------------------------
 
 $(if $(DEBUG),$(warning ENTER))
